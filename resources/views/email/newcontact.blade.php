94e79be93afffe0a510ae29726f4b51b9f2f19d6
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-10-04 18:20:57 +0200

    feat: add mail controller and view page

---
+++ resources/views/email/newcontact.blade.php
@@ -0,0 +1,4 @@
+<h1>Hai ricervuto questo messaggio</h1>
+<strong>Nome:</strong><span>{{ $lead->name }}</span>
+<strong>Email:</strong><span>{{ $lead->email }}</span>
+<strong>Messaggio:</strong><span>{{ $lead->message }}</span>
